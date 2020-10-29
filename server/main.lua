ea109c0c51a52216bac5f0d6f015e949ae44699b
Sander Jochems <sanderjochems@hotmail.nl>
2020-10-29 20:37:26 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -93,7 +93,7 @@ ESX.RegisterServerCallback('esx_ambulancejob:removeItemsAfterRPDeath', function(
 
 	if Config.RemoveBlackCashAfterRPDeath then
 		if xPlayer.getAccount('black_money').money > 0 then
-			xPlayer.triggerEvent('item_account', 'black_money', xPlayer.getAccount('black_money').money)
+			xPlayer.triggerEvent('esx:removeInventoryItem', 'item_account', 'black_money', xPlayer.getAccount('black_money').money)
 		end
 	end
 
